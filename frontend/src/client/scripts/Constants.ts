ff4f69e6442c12f88db7c297e0e95198a47b878f
Florian Gudat <florian.gudat@guices.com>
2019-12-03 19:06:03 +0100

    URL fix

--- frontend/src/client/scripts/Constants.ts
+++ frontend/src/client/scripts/Constants.ts
@@ -4,18 +4,18 @@ import { cfg } from "../../config";
 export const Applications = {
     MAIN: new EndpointProperties(
         cfg.CURRENT.options.text,
-        cfg.CURRENT.options.href.href
+        cfg.CURRENT.options.url.href
     ),
     ACCOUNT: new EndpointProperties(
         cfg.APPLICATIONS.Account.options.text,
-        cfg.APPLICATIONS.Account.options.href.href
+        cfg.APPLICATIONS.Account.options.url.href
     ),
     CATALOGUE: new EndpointProperties(
         cfg.APPLICATIONS.Catalogue.options.text,
-        cfg.APPLICATIONS.Catalogue.options.href.href
+        cfg.APPLICATIONS.Catalogue.options.url.href
     ),
     CHECKOUT: new EndpointProperties(
         cfg.APPLICATIONS.Checkout.options.text,
-        cfg.APPLICATIONS.Checkout.options.href.href
+        cfg.APPLICATIONS.Checkout.options.url.href
     )
 };
