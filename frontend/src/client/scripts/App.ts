6875dd89d6d893ccd581fb3379ef0237d7b30723
Florian Gudat <florian.gudat@guices.com>
2019-12-11 23:27:44 +0100

    Add ESI

--- frontend/src/client/scripts/App.ts
+++ frontend/src/client/scripts/App.ts
@@ -2,24 +2,24 @@ import {
     MessagingEndpoints,
     MessagingChannel,
     MessagingSystem,
-    LoggerSingleton
+    Logger
 } from "@scsa/messaging";
 
 export class App implements MessagingEndpoints {
     channel: MessagingChannel;
+    private logger: Logger;
 
     constructor(messagingSystem: MessagingSystem) {
         this.channel = messagingSystem.options.channel;
         this.channel.subscribe(this);
+        this.logger = new Logger();
     }
 
     handleEndpoint(data: any) {
         try {
-            const instance = LoggerSingleton.getInstance();
-            instance.write(data);
+            this.logger.write(data);
         } catch (error) {
             console.warn(error);
         }
     }
-
 }
